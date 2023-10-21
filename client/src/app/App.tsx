abc49015d2c84b1a6e0f4d4b61066dd4ce057c4b
a-zelenkov <zelenkov_a_m_01@mail.ru>
2023-10-21 11:53:28 +0300

    google oauth

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,20 @@
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "widgets/header";
 
 export function App() {
 	return (
-		<>
+		<GoogleOAuthProvider clientId="482536207010-gsiha5umtdd36su5o8kgmaunkv1kqr6m.apps.googleusercontent.com">
 			<AppHeader />
 			<Outlet />
-		</>
+			<GoogleLogin
+				onSuccess={credentialResponse => {
+					console.log(credentialResponse);
+				}}
+				onError={() => {
+					console.log("Login Failed");
+				}}
+			/>
+		</GoogleOAuthProvider>
 	);
 }
